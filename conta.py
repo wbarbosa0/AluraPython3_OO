6525e41d0977653a0ce5793affff24f1765b866d
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-12-23 23:47:50 -0200

    Aula 3 Atividade 1

--- conta.py
+++ conta.py
@@ -1,7 +1,15 @@
 class Conta:
-
     def __init__(self, numero, titular, saldo, limite=1000.0):
         self.numero = numero
         self.titular = titular
         self.saldo = saldo
         self.limite = limite
+
+    def extrato(self):
+        print("Saldo da conta {} de {} é de {}".format(self.numero,self.titular,self.saldo))
+
+    def deposita(self,valor):
+        self.saldo+=valor
+
+    def saca(self,valor):
+        self.saldo-=valor
