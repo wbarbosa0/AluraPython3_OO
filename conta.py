1103f8b19574d371bdf4d5cf714738404b1a2afe
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-12-23 23:00:40 -0200

    Aula 2 Atividade 8

--- conta.py
+++ conta.py
@@ -1,3 +1,7 @@
 class Conta:
 
-    pass
+    def __init__(self, numero, titular, saldo, limite=1000.0):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
