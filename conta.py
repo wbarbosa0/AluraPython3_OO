a03b9b5e280e1b634af84444cb0b54a9972c103f
wbarbosa0 <wjb00000000-github@yahoo.com.br>
2017-12-24 01:20:35 -0200

    Aula 5 Atividade 8

--- conta.py
+++ conta.py
@@ -1,7 +1,7 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite=1000.0):
         self.__numero = numero
-        self.__titular = titular
+        self.__titular = titular.title()
         self.__saldo = saldo
         self.__limite = limite
 
@@ -17,3 +17,19 @@ class Conta:
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite=limite
